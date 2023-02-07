646020dd8f82b4da6484ca3a06517d2152f56624
Eleneko <miraisoncloudi@gmail.com>
2023-02-07 19:30:43 -0300

    Aplicación vue

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const app = Vue.createApp({
-    template: `
-    <h1>Hola Mundo </h1>
-    <p> Desde app.js </p>
-    `
+    // template: `
+    // <h1> Hola Mundo </h1>
+    // <!-- Entre las llaves se pueden poner expresiones de js validas, no todas funcionan
+    //  por ejemplo el if no funciona pero podemos usar un ternario reemplazando el if
+    //  Se pueden llamar funciones-->
+  
+    // <p> {{ !true ? 'Activo': 'Inactivo' }} </p>
+    // <p> Desde app.js </p>
+    // `
 })
 
 app.mount('#myApp')
