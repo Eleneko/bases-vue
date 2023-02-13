44c2da5154fe2de42062ea720b7f4de7aec122b6
Eleneko <miraisoncloudi@gmail.com>
2023-02-13 19:30:01 -0300

    v-on y v-model

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
+const quotes = [
+    { quote: 'The night is darkest just before the dawn. And I promise you, the dawn is coming.', author: 'Harvey Dent, The Dark Knight' },
+    { quote: 'I believe what doesn’t kill you simply makes you, stranger.', author: 'The Joker, The Dark Knight' },
+    { quote: 'Your anger gives you great power. But if you let it, it will destroy you… As it almost did me', author: 'Henri Ducard, Batman Begins' },
+    { quote: 'You either die a hero or live long enough to see yourself become the villain.', author: 'Harvey Dent, The Dark Knight' },
+    { quote: 'If you’re good at something, never do it for free.', author: 'The Joker, The Dark Knight' },
+    { quote: 'Yes, father. I shall become a bat.', author: 'Bruce Wayne/Batman, Batman: Year One' },
+]
+
+
 const app = Vue.createApp({
-    // template: `
-    // <h1> Hola Mundo </h1>
-    // <!-- Entre las llaves se pueden poner expresiones de js validas, no todas funcionan
-    //  por ejemplo el if no funciona pero podemos usar un ternario reemplazando el if
-    //  Se pueden llamar funciones-->
-  
-    // <p> {{ !true ? 'Activo': 'Inactivo' }} </p>
-    // <p> Desde app.js </p>
-    // `
+
 
     data() {
         return {
-            // message: 'Hola mundo desde return de app owo'
-            quote: "Meow meow meow",
-            author: "Gato"
+            quotes,
+            newQuote:'Hola mundo'
         }
     },
     methods: {
-        changeQuote(){
-            console.log('Hola mundo uwu')
-            this.author = 'Gato enojado'
-            this.capitalize()
-        },
-        capitalize(){
-            this.quote = this.quote.toUpperCase()
+        addQuote( event ){
+            console.log(this.newQuote)
+            console.log(event)
+            
+            this.quotes.unshift({
+                quote: this.newQuote
+            })
         }
-    },
-        
-    
+    }
+ 
 })
 
 app.mount('#myApp')
